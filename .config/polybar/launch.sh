6a4289e4680a3551ff24197dd43b7a6575b98aca
Jake Addis <github@jakeaddis.com>
2021-05-26 21:11:32 -0500

    Add picom, alacritty is great aside from font, picom seems to be in a great place.  Rofi questionable, polybar terrible.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
+polybar left &
+polybar right &
+polybar centre &
 
 echo "Polybar launched..."
