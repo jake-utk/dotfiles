e6ffae5f1f2522d10dbcc4c37e35eafc34df0e5f
jake-utk <github@jakeaddis.com>
2021-12-10 00:27:37 -0600

    remove bkp polybar config, adjust polybar launch script, adjust restart_dunst script, adjust i3 and dunst configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,6 @@ killall -q polybar
 # while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-# polybar example >>/tmp/polybar1.log 2>&1 & disown
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload main &
