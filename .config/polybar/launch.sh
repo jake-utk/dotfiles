be70c7476d82d09712bf6d8f422434622cc47fae
jake-utk <github@jakeaddis.com>
2022-02-03 22:28:29 -0600

    adjust aliases, add nm-applet to i3, polybar placeholder

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,13 +4,13 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
   polybar --reload main --log=error &
 fi
