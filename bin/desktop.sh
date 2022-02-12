b79a7cfed7e57913dc438e28a56198ca63311e8e
jake-utk <github@jakeaddis.com>
2022-02-12 16:32:57 -0600

    remove desktop script

--- bin/desktop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --primary --mode 1920x1080 --pos 1080x1440 --rotate normal --output HDMI-1 --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x600 --rotate left --output DP-3 --off --output DP-4 --off
