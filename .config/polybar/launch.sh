b46f9fb3263bb6efb18d630e38081d7c3f031af0
jake-utk <github@jakeaddis.com>
2022-07-22 09:46:14 -0500

    small edits of polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar --reload main --log=error &
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload main &
-#   done
-# else
-  polybar --reload main --log=error &
-fi
+
+# for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+#     if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+#         export HWMON_PATH="$i"
+#     fi
+# done
